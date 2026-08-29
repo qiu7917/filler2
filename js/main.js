f0a11c15526d49f102d5f0a06bdbdb20112fe1b9
qiu7917 <chupa_p@hotmail.com>
2026-08-29 19:53:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,35 +20,6 @@ $(function () {
     /*=================================================
     アコーディオンメニュー
     ===================================================*/
-    // $('.accordion-header').on('click', function () {
-    //     var $header = $(this);
-    //     var $item = $header.parent(); 
-    //     var $content = $header.next('.accordion-content');
-    //     var $slider = $header.closest('.slick-slider'); 
-
-    //     if ($header.hasClass('is-active')) {
-    //         $header.removeClass('is-active');
-    //         $item.removeClass('is-open-padding');
-    //         $content.slideUp(400, function() {
-    //             if ($slider.length) {
-    //                 $slider.slick('setPosition');
-    //             }
-    //         });
-    //     } else {
-    //         $header.addClass('is-active');
-    //         $item.addClass('is-open-padding');
-    //         $content.slideDown(400, function() {
-    //             if ($slider.length) {
-    //                 $slider.slick('setPosition');
-    //             }
-    //             var $iframe = $content.find('iframe');
-    //             if ($iframe.length && !$iframe.attr('src')) {
-    //                 $iframe.attr('src', $iframe.data('src'));
-    //             }
-    //         });
-    //     }
-    // });
-
     $('.accordion-header').on('click', function () {
         var $header = $(this);
         var $item = $header.parent(); 
